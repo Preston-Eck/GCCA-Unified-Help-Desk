90f94233cf35e5f3009eae68754ada174c1c9a2c
Preston E. <preston@udrg.us>
2025-12-05 15:33:43 -0500

    Deployment Prep

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
-
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { viteSingleFile } from "vite-plugin-singlefile";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    viteSingleFile()
-  ],
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '');
+  return {
+    plugins: [react(), viteSingleFile()],
+    build: {
+      target: "esnext",
+      assetsInlineLimit: 100000000,
+      chunkSizeWarningLimit: 100000000,
+      cssCodeSplit: false,
+      reportCompressedSize: false,
+      rollupOptions: {
+        output: {
+          inlineDynamicImports: true,
+        },
+      },
+    },
+  };
 });
